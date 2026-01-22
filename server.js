4de4ff7fcb9f421a69b9db6886601b9cb2bb1cec
Amruth0_0 <ammu.ventures@gmail.com>
2026-01-22 23:33:53 +0530

    chore: set up server config and required dependencies

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const path = require('path');
+const dotenv  = require('dotenv')
+const dbConnect = require('./config/db')
 const indexRouter = require('./routes/main.route');
-const app = express();
 
-// serve static assets from <project>/public so /assets/Logo.svg -> public/assets/Logo.svg
+
+const app = express();
+dotenv.config();
+dbConnect()
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', indexRouter);
+app.use('/register', indexRouter)
+
 
 app.listen(3000, () => {
     console.log("Server Running on Port 3000");
